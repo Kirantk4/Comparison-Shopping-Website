b9ea8109d1209c630f21cf53fe8ba05decde9b34
Kiran-K4 <s3943716@student.rmit.edu.au>
2023-11-20 13:22:02 +1100

    Clean-up Part-7

--- frontend/comparison-shopper/src/Modals/signUpModal.js
+++ frontend/comparison-shopper/src/Modals/signUpModal.js
@@ -1,7 +1,5 @@
 import React, {useState, useContext} from 'react';
-import { useNavigate } from "react-router-dom";
-// import {signupVerify} from "../Data/repository.js";
-import { Modal, Container } from 'react-bootstrap';
+import { Modal } from 'react-bootstrap';
 import {createNewUser} from '../Data/repository.js';
 import {UserContext} from '../App.js';
 
@@ -17,6 +15,7 @@ const SignUpModal = (props) => {
   async function handleSubmit(event) {
     event.preventDefault();
     //Regex that should cover 99.99% of emails to validate email inputs.
+    // eslint-disable-next-line no-control-regex
     const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
     if (!emailRegex.test(emailInput)){
       setErrorDetected("Email does not meet formatting standards.");
